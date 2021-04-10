05ce5ca036f910cedba71b23d254165780f64ef6
Eralp Orkun <eaorkun@gmail.com>
2021-04-10 14:08:01 -0500

    Fix Naming convention

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -1,6 +1,26 @@
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ClientMain
 {
-   public static void main(String[] args) {
-      System.out.println("Hello World");
+   public static void main(String[] args)
+   {
+      try
+      {
+         Socket s = new Socket("localhost", 6666);
+
+         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+         dout.writeUTF("Hello Server");
+         dout.flush();
+
+         dout.close();
+         s.close();
+
+      }
+      catch (Exception e)
+      {
+         System.out.println(e);
+      }
    }
 }
